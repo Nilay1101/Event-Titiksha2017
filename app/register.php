7819815b8bddcdc15ba77c87a1306718e55ec95a
Nilay Jayswal <nilayjayswalnj@gmail.com>
2016-10-19 01:01:49 +0530

    register page

---
+++ app/register.php
@@ -0,0 +1,30 @@
+<?php	
+if(empty($_POST['name'])  		||
+   empty($_POST['gender']) 		||
+   empty($_POST['email']) 		||
+   empty($_POST['ins'])	||
+   empty($_POST['pin'])	||
+   empty($_POST['phone'])	||
+   empty($_POST['message'])	||
+   !filter_var($_POST['email'],FILTER_VALIDATE_EMAIL))
+   {
+	echo "No arguments Provided!";
+	return false;
+   }
+   
+$name = strip_tags(htmlspecialchars($_POST['name']));
+$gender = strip_tags(htmlspecialchars($_POST['gender']));
+$email = strip_tags(htmlspecialchars($_POST['email']));
+$ins = strip_tags(htmlspecialchars($_POST['ins']));
+$pin = strip_tags(htmlspecialchars($_POST['pin']));
+$phone = strip_tags(htmlspecialchars($_POST['phone']));
+$message = strip_tags(htmlspecialchars($_POST['message']));
+
+$to = 'titiksha@smvdu.ac.in'; 
+$email_subject = "TITIKSHA Registartion Form:  $name";
+$email_body = "You have received a new registartion update.\n\n"."Here are the details:\n\nName: $name\n\nGender: $gender\n\nEmail: $email\n\nInstitution: $ins\n\nPincode: $pin\n\nPhone: $phone\n\nMessage:\n$message";
+$headers = "From: noreply@yourdomain.com\n"; 
+$headers .= "Reply-To: $email";	
+mail($to,$email_subject,$email_body,$headers);
+return true;			
+?>
